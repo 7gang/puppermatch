3e8bc41a010c6f20d0390c4092a9d97a62dd58a7
kuff <16867891+kuff@users.noreply.github.com>
2021-04-19 11:48:15 +0200

    Set up initial client request

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,1 +1,16 @@
-console.log("Client script executed sucessfully!");
+function runGame(gameState) {
+    var elem = document.getElementById("connectionBox");
+    elem.parentNode.removeChild(elem);
+
+    console.log('game data retrieved sucessfully: ', gameState);
+}
+
+fetch('http://localhost:3000/api/getgame')
+    .then(result => result.json())
+    .then(data => runGame(data))
+    .catch(error => {
+        var $container = document.querySelector('#connectionBox');
+        var newHTML = '<h1>Could not connect to game servers!</h1>' +
+            '<p>Verify your internet connection or check back later!</p>';
+        $container.innerHTML = newHTML;
+    });
